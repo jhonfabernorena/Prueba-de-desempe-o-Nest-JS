3128b7d818e80b3ba273b16e77dc8b434d5b2988
jhonfabernorena <jhonfabernorenapineres@gmail.com>
2024-06-04 08:15:09 -0500

    se hace la conexion en postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlayersController } from './models/players/controllers/player.controller';
+import { PlayersEntity } from './models/players/entities/players.entity';
+import { PlayersService } from './models/players/services/player.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, 
+      entities: [PlayersEntity],
+      extra: {
+        ssl: true, 
+      },
+    }),
+    TypeOrmModule.forFeature([PlayersEntity]),
+  ],
+  controllers: [PlayersController],
+  providers: [PlayersService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('')
+    .setDescription(`Descripción.`)
+    .setVersion('1.0')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(3000);
+
+  console.log("Project started => localhost:3000");
+  console.log("=========================================================");
+  console.log("Access to the project via Swagger: localhost:3000/api-doc");
 }
 bootstrap();
+
